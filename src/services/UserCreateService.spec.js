286cd9a2192c9aae76293e19280c0cd095b20b26
Marcos Colella <colella.dev@gmail.com>
2024-02-07 17:49:37 -0300

    Adicionando o describe no test

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,17 +1,19 @@
-    const UserCreateService = require("./UserCreateService")
-    const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
-    
-    it("user should be create", async () => {
-      const user = {
-        name: "User Test",
-        email: "user@test.com",
-        password: "123"
-      }
+const UserCreateService = require("./UserCreateService")
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory") //Importa
 
-    const userRepositoryInMemory = new UserRepositoryInMemory()  
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
-    const userCreated =  await userCreateService.execute(user)  
-
-    console.log(userCreated)
-    expect(userCreated).toHaveProperty("id")
-    }) 
+describe("UserCreateService", () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    }
+  
+  const userRepositoryInMemory = new UserRepositoryInMemory() // Instancia
+  const userCreateService = new UserCreateService(userRepositoryInMemory) // Passa aqui
+  const userCreated = await userCreateService.execute(user)  
+  
+  console.log(userCreated)
+  expect(userCreated).toHaveProperty("id")
+  }) 
+})
