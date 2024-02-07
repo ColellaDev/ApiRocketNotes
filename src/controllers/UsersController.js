52cda8a80e49c9a6314e5ac63764997ee03f5eca
Marcos Colella <colella.dev@gmail.com>
2024-02-07 13:58:01 -0300

    Aplicando Inversão de Dependencias

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,25 +1,19 @@
 const { hash, compare} = require("bcryptjs")
 const AppError = require("../utils/AppError")
+
 const sqliteConnection = require("../database/sqlite")
 const knex = require("../database/knex")
 
 const UserRepository = require("../repositories/UserRepository")
+const UserCreateService = require("../services/UserCreateService")
 
 class UsersController {
     async create (request, response) {
     const {name, email, password} = request.body
 
     const userRepository = new UserRepository()
-    
-    const checkUsersExists = await userRepository.findByEmail(email)
-
-    if (checkUsersExists){
-        throw new AppError("Este e-mail já está em uso.")
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await userRepository.create({name, email, password: hashedPassword})
+    const userCreateService = new UserCreateService(userRepository)
+    await userCreateService.execute({name, email, password})
 
     return response.status(201).json();
     
