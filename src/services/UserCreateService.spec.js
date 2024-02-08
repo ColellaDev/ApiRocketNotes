104b41736314495544dc8af389ccc4507191684c
Marcos Colella <colella.dev@gmail.com>
2024-02-08 09:30:46 -0300

    Criando Test E-mail em uso

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,7 +1,9 @@
 const UserCreateService = require("./UserCreateService")
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory") //Importa
+const AppError = require("../utils/AppError")
 
 describe("UserCreateService", () => {
+
   it("user should be create", async () => {
     const user = {
       name: "User Test",
@@ -16,4 +18,24 @@ describe("UserCreateService", () => {
   console.log(userCreated)
   expect(userCreated).toHaveProperty("id")
   }) 
+
+  it("user not should be create with exists email", async() => {
+    const user1 = {
+      name: "User Teste 1",
+      email: "user@teste.com",
+      password: "123"
+    }
+
+    const user2 = {
+      name: "User Teste 2",
+      email: "user@teste.com",
+      password: "456"
+    }
+
+    const userRepository = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepository)
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual( new AppError("Este e-mail já está em uso."))
+  })
 })
